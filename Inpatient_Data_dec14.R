5c5c350aa49759271a424cf6d9467b0149bf67f8
adiamb <ambati@psyc-ml-imac2.stanford.edu>
2016-12-15 12:10:22 -0800

    update1
    Modeling routines now integrated

--- Inpatient_Data_dec14.R
+++ Inpatient_Data_dec14.R
@@ -5,20 +5,32 @@ require(readr)
 require(ggplot2)
 inpatient = read_csv("https://data.cms.gov/api/views/97k6-zzx3/rows.csv?accessType=DOWNLOAD") #read in the file from data.gov
 dim(inpatient) #should be 16K rows 
-apply(inpatient, 2, FUN = function(x) (table(is.na(x)))) #check for NAs in the dataset
+apply(inpatient, 2, FUN = function(x) (sum(is.na(x)))) #check for NAs in the dataset
 inpatient=inpatient[-c(4, 7)] ## remove unnecessary columns 
 names(inpatient)=gsub(" ", "_", names(inpatient))  #lets us put underscore in colnames for sake simplicity
-inpatient$`Average Covered Charges`=as.numeric(gsub("\\$", "", inpatient$`Average Covered Charges`))/1000 #convert to numeric and divide by 1000 
-inpatient$`Average Total Payments`=as.numeric(gsub("\\$", "", inpatient$`Average Total Payments`))/1000 #convert to numeric and divide by 1000 
-inpatient$`Average Medicare Payments`=as.numeric(gsub("\\$", "", inpatient$`Average Medicare Payments`))/1000 #convert to numeric and divide by 1000 
+inpatient$Average_Covered_Charges=as.numeric(gsub("\\$", "", inpatient$Average_Covered_Charges)) #convert to numeric 
+inpatient$Average_Total_Payments=as.numeric(gsub("\\$", "", inpatient$Average_Total_Payments)) #convert to numeric 
+inpatient$Average_Medicare_Payments=as.numeric(gsub("\\$", "", inpatient$Average_Medicare_Payments)) #convert to numeric
+inpatient$DRG_Definition = as.factor(inpatient$DRG_Definition)
+inpatient$Provider_State = as.factor(inpatient$Provider_State)
 str(inpatient) #check 
 summary(inpatient)
 ggplot(inpatient, aes(factor(inpatient$`DRG Definition`), inpatient$`Average Total Payments`))+geom_boxplot()+theme(axis.text.x = element_text(angle = 90))+coord_flip()
 ggplot(inpatient, aes(factor(inpatient$`Provider State`), inpatient$`Average Covered Charges`))+geom_boxplot()+theme(axis.text.x = element_text(angle = 90))+coord_flip()
 
-setDT(inpatient)
+setDT(inpatient) ## change to data table, makes it easy to aggregate
 inpat_agg=inpatient[, list(Total_Discharges = mean(Total_Discharges), Average_Covered_Charges = mean(Average_Covered_Charges), Average_Total_Payments = mean(Average_Covered_Charges), Average_Medicare_Payments = mean(Average_Medicare_Payments)), by=list(DRG_Definition, Provider_State)]
 ggplot(inpat_agg, aes(factor(DRG_Definition), Average_Medicare_Payments))+geom_bar(stat = "identity", color = factor())+theme(axis.text.x = element_text(angle = 90, size = 10, face = "bold"), axis.text.y = element_text(size = 10, face = "bold"))+coord_flip()
 ggplot(inpat_agg, aes(factor(DRG_Definition), Average_Medicare_Payments, fill = factor(Provider_State)))+geom_bar(stat = "identity")+theme(axis.text.x = element_text(angle = 90, size = 10, face = "bold"), axis.text.y = element_text(size = 10, face = "bold"))+coord_flip()
-ggplot(inpat_agg, aes(factor(Provider_State), Average_Medicare_Payments)+geom_bar(stat = "identity")+theme(axis.text.x = element_text(angle = 90, size = 10, face = "bold"), axis.text.y = element_text(size = 10, face = "bold"))+coord_flip()
-       
+ggplot(inpat_agg, aes(factor(Provider_State), Average_Medicare_Payments))+geom_bar(stat = "identity")+theme(axis.text.x = element_text(angle = 90, size = 10, face = "bold"), axis.text.y = element_text(size = 10, face = "bold"))+coord_flip()
+
+###in california lets us look at to which condition most medicare payments were done !
+ggplot(filter(inpat_agg, Provider_State == "CA"), aes(factor(DRG_Definition), Average_Total_Payments))+geom_bar(stat = "identity")+theme(axis.text.x = element_text(angle = 90, size = 10, face = "bold"), axis.text.y = element_text(size = 8, face = "bold"))+coord_flip()
+
+###build a glm model to predict prices in the large dataset given the condition and the referral region
+model1=glm(Average_Total_Payments ~ DRG_Definition+Hospital_Referral_Region_Description, data = inpatient) #family = "gaussian")
+test=inpatient[15000:15010, 1:ncol(inpatient), with = F] ##select a test row from the main dataset
+test$Average_Total_Payments = 0 ##assign zero to the output variable
+predict(model1, test)
+
+
